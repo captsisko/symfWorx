08fe072ab925578a8fd5782f3cb138cb7ec4fa88
Sisko <adeoduye@gmail.com>
2019-09-21 14:41:46 +0000

    Implements fizzbuzz requirement displaying output via a twig template

--- src/Controller/FizzBuzzController.php
+++ src/Controller/FizzBuzzController.php
@@ -4,14 +4,17 @@
 
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
+    // use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
-
-    class FizzBuzzController{
+    class FizzBuzzController extends Controller{
 
         /**
          * @Route("/")
          */
         function index(){
-            return new Response("testing ... testing");
+            // return new Response("testing ... testing");
+            return $this->render('fizzbuzz.html.twig');
         }
     }
