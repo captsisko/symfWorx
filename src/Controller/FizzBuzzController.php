8c87d4a7abab400be1a149bf2338e62261d06221
Sisko <adeoduye@gmail.com>
2019-09-21 14:52:21 +0000

    Corrects styling link and cleans up the code in different files

--- src/Controller/FizzBuzzController.php
+++ src/Controller/FizzBuzzController.php
@@ -4,7 +4,6 @@
 
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-    // use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
